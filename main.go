97c930b7625d423772d641ee1bf29c4f56fce317
Brandon Kurtz <brandon.kurtz@airbnb.com>
2021-09-04 16:07:02 -0700

    adds isHoliday endpoint

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/google/logger"
 	"github.com/gorilla/mux"
@@ -23,6 +24,14 @@ type Holiday struct {
 	Types       *[]string `json:"types"`
 }
 
+func mapOfHolidays(holidays *[]Holiday) map[string]Holiday {
+	hm := map[string]Holiday{}
+	for _, holiday := range *holidays {
+		hm[*holiday.Date] = holiday
+	}
+	return hm
+}
+
 func getPublicHolidays(year string, countryCode string) (*[]Holiday, error) {
 	url := fmt.Sprintf(
 		"https://date.nager.at/api/v3/publicholidays/%s/%s",
@@ -37,6 +46,7 @@ func getPublicHolidays(year string, countryCode string) (*[]Holiday, error) {
 		return nil, err
 	}
 	if resp.StatusCode != http.StatusOK {
+		logger.Error(string(byt))
 		return nil, fmt.Errorf("resp.Status was: %d", resp.StatusCode)
 	}
 	var holidays *[]Holiday
@@ -68,9 +78,36 @@ func HolidaysHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(byt)
 }
 
+func IsHolidayHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	countryCode := vars["countryCode"]
+	date := vars["date"]
+	year := strings.Split(date, "-")
+	logger.Infof("year: %s", year)
+	holidays, err := getPublicHolidays(year[0], countryCode)
+	if err != nil {
+		logger.Error(err)
+	}
+	hm := mapOfHolidays(holidays)
+	if val, ok := hm[date]; ok {
+		byt, err := json.Marshal(val)
+		if err != nil {
+			logger.Error(err)
+		}
+		w.Write(byt)
+		return
+	}
+	msg := fmt.Sprintf("no holidays on %s", date)
+	w.Write([]byte(msg))
+}
+
+var (
+	serverURL = "localhost:8080"
+)
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/holidays/{year}/{countryCode}", HolidaysHandler)
-	logger.Fatal(http.ListenAndServe("localhost:8080", r))
-
+	r.HandleFunc(("/isHoliday/{date}/{countryCode}"), IsHolidayHandler)
+	logger.Fatal(http.ListenAndServe(serverURL, r))
 }
